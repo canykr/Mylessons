a6ed9f81cbb529f55677e5b9dc7fcc4f0bf19f97
Can Yakar <canykr@hotmail.com>
2023-11-09 22:26:25 +0300

    Ders1

--- C#-Programlama-Dili/01-VeriTipleriVeDegiskenler/Program.cs
+++ C#-Programlama-Dili/01-VeriTipleriVeDegiskenler/Program.cs
@@ -5,6 +5,42 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            //ctrl+k+d kodların sıralamasını düzenler
+
+            int sayi1 = 999;
+            int sayi2 = 999;
+
+            int sayi3, sayi4; // değerleri 0'dır.
+            sayi3 = 5;
+            sayi3 = 45;
+
+
+            decimal virgulluSayi1 = 2.5M;
+            decimal virgulluSayi2 = 2205;
+            // decimal virgulluSayi1= 2.5m;
+
+            float virgulluSayi3 = 1.53F;
+            float virgulluSayi4 = 1;
+            //float virgulluSayi2=1.53f;
+
+            double virgulluSayi5 = 5.5D;
+            //double virgulluSayi3= 5.5d;
+            //double virgulluSayi3= 5.5;
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
