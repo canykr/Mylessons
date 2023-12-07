b7bc38334a5520eebd8a6d12998cdf28f16f2cbd
Can Yakar <canykr@hotmail.com>
2023-12-07 19:33:08 +0300

    12.07.2023

--- C#-Programlama-Dili/15_StringMethodlari/Program.cs
+++ C#-Programlama-Dili/15_StringMethodlari/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace _15_StringMethodlari
+﻿using System.Net.Http.Headers;
+
+namespace _15_StringMethodlari
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
             string _adiSoyadi = " Aykut Baştuğ ";
             string adiSoyadi1 = _adiSoyadi.Trim();
             string adiSoyadi2 = _adiSoyadi.TrimEnd(); // Sadece sondaki boşlukları siler
@@ -36,7 +39,109 @@
             // PadRight() metnin istediğimiz karakter sayısına ulaşana kadar sağ tarafına değer ekliyor.
             string faturaNo2 = _faturaNo.PadRight(4, '0');
 
+            //Equals() parametre olarak verilen deger ile degiskenin8 degeri aynı mı degil mi diye kontrol eder. (eşit mi değil mi kontrolünü yapıyor) örnekteki if elsenin işini yapıyor.
+            string _metin = "kıyaslamak icin kullanılır";
+            bool esitMi = _metin.Equals("icin");
+            bool esitMi1 = _metin.Equals("kıyaslamak icin kullanılır");
 
+            if (_metin == "kıyaslamak icin kullanılır")
+                esitMi1 = true;
+            else
+                esitMi1 = false;
+
+
+            //SubString() metodu string bir deger icerisinden parca almak icin kullanılır.
+            string _ornek = "Örnek metni buraya giriyorum.";
+            string ornek1 = _ornek.Substring(5); //sadece index konumundan itibaren alır.
+            string ornek2 = _ornek.Substring(5, 4); // 5. indexten sonra 4 karakter alır.
+
+            //Reverse() metodu metnin icindeki karakterleri bir dizi haline getirerek tersten sıralar.
+            string _terstenMetin = "Aykut BAŞTUĞ";
+            char[] metinDizi = _terstenMetin.Reverse().ToArray();
+            string terstenMetin = "";
+            foreach (var item in metinDizi)
+            {
+                terstenMetin += item.ToString();
+                Console.WriteLine(_terstenMetin);
+            }
+
+            //IndexOf() metodu metnin icindeki bir karakterin ya da kelimenin kacıncı indexte oldugunu bize döner int olarak.
+            string notebook = "Asus Zenbook";
+            int index1 = notebook.IndexOf('o');
+            int index2 = notebook.IndexOf("Zen");
+            int index3 = notebook.IndexOf("zen"); // karsılıgını bulamadıgı için -1 olarak döner.
+
+            //Remove() metodu metnin icindeki herhangi bir indexten itibaren karakterleri silmek icin kullanılır.
+            string _mevsim = "Ağustos";
+            string mevsim1 = _mevsim.Remove(3);
+            string mevsim2 = _mevsim.Remove(3, 2);
+            // string mevsim3 =_mevsim.Remove(13,1); // eger olmayan bir index numarası verilirse kod satırı hata verir.
+
+            //Contains() metodu metinin içinde bir karakter ya da kelimeyi içeriyor mu diye kontrol etmek için kullanılır.
+            string marka = "philips";
+            marka.Contains('i');
+            bool iceriyorMu = marka.Contains("i");
+            bool iceriyorMu2 = marka.Contains("lip");
+            bool iceriyorMu3 = marka.Contains("olip");
+            bool iceriyorMu4 = marka.Contains("o");
+            bool iceriyorMu5 = marka.Contains("İ");  // büyük kücük harf duyarlılıgı var.
+
+            // StartsWith() metodu metnin herhangi bir karakter yada kelime ile başlayıp başlamadığını kontrol ediyor.
+            string meyve = "Portakal";
+            bool il_eBasliyorMu1 = meyve.StartsWith('p');
+            bool il_eBasliyorMu2 = meyve.StartsWith('P');
+            bool il_eBasliyorMu3 = meyve.StartsWith('x');
+            bool il_eBasliyorMu4 = meyve.StartsWith("Por");
+            bool il_eBasliyorMu5 = meyve.StartsWith("por");
+
+
+            // EndsWith() metodu metnin herhangi bir karakter yada kelime ile bitip bitmediğini kontrol ediyor.
+            bool il_eBitiyorMu1 = meyve.EndsWith('l');
+            bool il_eBitiyorMu2 = meyve.EndsWith('L');
+            bool il_eBitiyorMu3 = meyve.EndsWith('x');
+            bool il_eBitiyorMu4 = meyve.EndsWith("kal");
+            bool il_eBitiyorMu5 = meyve.EndsWith("Kal");
+
+            //IsNullOrEmpty() metodu metnin NULL veya bos olup olmadıgını kontrol eder. NULL veya bos degil ise False degeri döndürür.
+            //string isim = "";
+            string? isim = null;
+            isim = "Aykut";
+
+            if (string.IsNullOrEmpty(isim) == true)
+                Console.WriteLine("İsim girilmedi!");
+            else
+                Console.WriteLine("Merhaba " + isim);
+
+            // kullanıcıdan 3 tane isim istesin isim girilmediyse isim girilmediğiniz uyarısını versin tekrar girmesini sağlasın, 3 ismi girdikten sonra M harfi ile başlayanları alt alta ekrana yazdırsın.
+            string[] isimler = new string[3];
+
+            for (int i = 0; i < isimler.Length; i++)
+            {
+                string isim_;
+                do
+                {
+                    Console.Write($"Lütfen {i + 1}. ismi giriniz (sadece harfler olmalı): ");
+                    isim_ = Console.ReadLine();
+
+                    // Boş kontrolü ve sadece harflerden oluşup oluşmadığının kontrolü
+                    if (string.IsNullOrEmpty(isim_) || !IsAllLetters(isim_))
+                    {
+                        Console.WriteLine("Geçersiz giriş. Lütfen boş bırakmayın ve sadece harf giriniz.");
+                    }
+                }
+                while (string.IsNullOrEmpty(isim_) || !IsAllLetters(isim_));
+
+                isimler[i] = isim_;
+            }
+
+            Console.WriteLine("\n'M' harfi ile başlayan isimler:");
+            foreach (var isim_ in isimler)
+            {
+                if (isim.ToUpper().StartsWith("M"))
+                {
+                    Console.WriteLine(isim_);
+                }
+            }
         }
     }
 }
---
+++ C#-Programlama-Dili/17_MathMetotlari/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _17_MathMetotlari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int enBuyuk = Math.Max(5, 10);
+            int enBuyuk2 = Math.Max(18, Math.Max(5, 10));
+            Console.WriteLine("Büyük Sayi = " + enBuyuk);
+            Console.WriteLine("Büyük Sayı = " + enBuyuk2);
+
+            // Math.Min() En küçük değeri bulmak için kullanılır
+            int enKucuk = Math.Min(5, 10);
+            int enKucuk2 = Math.Min(18, Math.Min(5, 10));
+            Console.WriteLine("Küçük Sayı = " + enKucuk);
+            Console.WriteLine("Küçük Sayı = " + enKucuk2);
+
+            // Math.Sqrt(x) parametre olarak verilen x sayısının karekökünü hesaplayarak geriye döner
+            double kareKok = Math.Sqrt(64);
+            Console.WriteLine("64'ün kare kökü = " + kareKok);
+
+            //Math.Abs(x) parametre olarak verilen x degerinin mutlak ( pozitif ) karsılıgını geriye döndürüyor
+            int mutlakDeger = Math.Abs(-14);
+            double mutlakDeger2 = Math.Abs(-14.5);
+            Console.WriteLine("-14'ün mutlak degeri =" +mutlakDeger);
+            Console.WriteLine("-14.5'in mutlak degeri =" +mutlakDeger2);
+
+            //Math.Round(x) kendisine en yakın TAM SAYIYA yuvarlar
+            double yuvarla1 = Math.Round(7.99);
+            double yuvarla2 = Math.Round(7.19);
+
+            double yuvarla3 = Math.Round(4.1245,2); // Virgulden sonra 2 digit kadar alır
+            double yuvarla4 = Math.Round(4.1245, 1); 
+            double yuvarla5 = Math.Round(4.1245, 0); // tamsayıya yuvarlar
+
+            //Math.Floor(x) parametre olarak verilen sayıyı bir alt TAM SAYIYA yuvarlar. Yani virgülü atıyor diyebiliriz normalde 8'e yuvarlaması gerekirken alt sayı 7 ye yuvarlar
+            double asagiYuvarla = Math.Floor(7.99);
+
+            //Math.Ceiling(x) parametre olarak verilen sayıyı bir üst TAM SAYIYA yuvarlar. Yani virgülü atıyor diyebiliriz normalde 'e yuvarlaması gerekirken alt sayı 8 ye yuvarlar
+            double yukariYuvarla = Math.Ceiling(7.12);
+
+            //Pi sayisi
+            double piSayisi = Math.PI;
+            Console.WriteLine("Pi Sayısı = " + piSayisi);
+
+
+        }
+    }
+}
